505b4f5a85d3e88a138009dbe920767d0f2b28cd
SlavicJan <svyatoslav263@gmail.com>
2025-09-04 17:44:11 +0300

    correction

--- ARPU расчет.sql
+++ ARPU расчет.sql
@@ -1,4 +1,4 @@
 select round(avg(sum),3) from (select user_id, sum(value)
 from "transaction" t 
-where type_id in (2,23,24,25,26,27,28)
+where type_id in (1,23,24,25,26,27,28)
 group by user_id) arpu
